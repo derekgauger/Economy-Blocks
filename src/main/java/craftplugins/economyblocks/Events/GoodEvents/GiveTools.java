31fd0476cd0e272e02ce1c759a416b3d3965b43d
derekgauger <107506713+derekgauger@users.noreply.github.com>
2022-08-04 11:09:03 -0500

    Update GiveTools.java

--- src/main/java/craftplugins/economyblocks/Events/GoodEvents/GiveTools.java
+++ src/main/java/craftplugins/economyblocks/Events/GoodEvents/GiveTools.java
@@ -23,47 +23,47 @@ public class GiveTools implements CarePackageEvent {
 
         if (randomNum > 99) {
             ItemStack giveItem = createItem(Material.NETHERITE_PICKAXE, Utils.chat("&fNetherite Pickaxe"), 1, new Enchantment[]{Enchantment.DURABILITY, Enchantment.DIG_SPEED, Enchantment.LOOT_BONUS_BLOCKS}, new int[]{3, 5, 3});
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given an enchanted netherite pickaxe"));
 
         } else if (randomNum > 98) {
             ItemStack giveItem = createItem(Material.DIAMOND_PICKAXE, Utils.chat("&fDiamond Pickaxe"), 1, new Enchantment[]{Enchantment.DURABILITY, Enchantment.DIG_SPEED, Enchantment.LOOT_BONUS_BLOCKS}, new int[]{3, 5, 3});
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given an enchanted diamond pickaxe"));
 
         } else if (randomNum > 95) {
             ItemStack giveItem = createItem(Material.DIAMOND_PICKAXE, Utils.chat("&fDiamond Pickaxe"), 1, new Enchantment[]{Enchantment.SILK_TOUCH}, new int[]{1});
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given a silk touch diamond pickaxe"));
 
         } else if (randomNum > 92) {
             ItemStack giveItem = createItem(Material.NETHERITE_PICKAXE, Utils.chat("&fNetherite Pickaxe"), 1, null, null);
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given a netherite pickaxe"));
 
         } else if (randomNum > 87) {
             ItemStack giveItem = createItem(Material.DIAMOND_PICKAXE, Utils.chat("&fDiamond Pickaxe"), 1, null, null);
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given a diamond pickaxe"));
 
         } else if (randomNum > 80) {
             ItemStack giveItem = createItem(Material.NETHERITE_SHOVEL, Utils.chat("&fNetherite Shovel"), 1, null, null);
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given a netherite shovel"));
 
         } else if (randomNum > 73) {
             ItemStack giveItem = createItem(Material.DIAMOND_SHOVEL, Utils.chat("&fDiamond Shovel"), 1, null, null);
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given a diamond shovel"));
 
         } else if (randomNum > 60) {
             ItemStack giveItem = createItem(Material.NETHERITE_HOE, Utils.chat("&fNetherite Hoe"), 1, new Enchantment[]{Enchantment.DURABILITY}, new int[]{3});
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given a netherite hoe"));
 
         } else if (randomNum > 40) {
             ItemStack giveItem = createItem(Material.DIAMOND_HOE, Utils.chat("&fDiamond Hoe"), 1, new Enchantment[]{Enchantment.DURABILITY}, new int[]{3});
-            player.getInventory().addItem(giveItem);
+            Utils.addItemToInventory(giveItem, player);
             player.sendMessage(Utils.chat("&dYou have been given a diamond hoe"));
 
         } else {
@@ -71,10 +71,10 @@ public class GiveTools implements CarePackageEvent {
             ItemStack ironAxe = createItem(Material.IRON_AXE, Utils.chat("&fIron Axe"), 1, null, null);
             ItemStack ironShovel = createItem(Material.IRON_SHOVEL, Utils.chat("&fIron Shovel"), 1, null, null);
             ItemStack ironHoe = createItem(Material.IRON_HOE, Utils.chat("&fIron Hoe"), 1, null, null);
-            player.getInventory().addItem(ironPick);
-            player.getInventory().addItem(ironAxe);
-            player.getInventory().addItem(ironShovel);
-            player.getInventory().addItem(ironHoe);
+            Utils.addItemToInventory(ironPick, player);
+            Utils.addItemToInventory(ironAxe, player);
+            Utils.addItemToInventory(ironShovel, player);
+            Utils.addItemToInventory(ironHoe, player);
             player.sendMessage(Utils.chat("&dYou have been given a set of iron tools"));
 
         }
